c782fff27969b5273570177e6b2cab635989d3d4
SELVABHARATHI S <sselvabharathi1512@gmail.com>
2025-08-02 16:49:39 +0530

    Delete middleware directory

--- middleware/admin.js
+++
@@ -1,8 +0,0 @@
-function isAdmin(req, res, next) {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ msg: 'Admin access only' });
-  }
-  next();
-}
-
-module.exports = isAdmin;
